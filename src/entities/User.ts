e3b687c1cb0139eae964bccea7df6e40a22bc0dc
Julio Tavares <jctbs@cin.ufpe.br>
2021-12-04 22:03:12 -0300

    criando entidade User

---
+++ src/entities/User.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryColumn, Column } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity("users")
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
